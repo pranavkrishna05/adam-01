2041c93eedfcbbc4c03315bfc2bcd3756000a5ca
pranavkrishna05 <pranav2002krishna@gmail.com>
2025-12-31 10:01:54 +0530

    Update main.jsx

--- app/Frontend/main.jsx
+++ app/Frontend/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import { CartProvider } from "./context/CartContext";
-import "./index.css";          
+import "./index.css";
 import "./styles/global.css"; 
 
 
